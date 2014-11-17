e3a403d96a685901e60e0d2516cfea793135a816
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2014-11-17 05:52:29 -0600

    memory reading is all pretty and dispatched now

--- memory.c
+++ memory.c
@@ -5,37 +5,46 @@
 
 unsigned char memory[0x10000];
 
-uint8_t mem_read8(uint16_t location)
+
+/***********************************************************************
+ * readers
+ ***********************************************************************/
+typedef uint8_t (*mem_read_fn)(uint16_t);
+
+uint8_t direct_read(uint16_t location)
 {
-	/* ROM bank #0 */
-	if(location < 0x4000)
-	{
-		return memory[location];
-	}
-	/* switchable ROM bank */
-	if(location < 0x8000)
-	{
-		fatal("bank switching not yet implemented");
-	}
-	/* video memory */
-	if(location < 0xA000)
-	{
-		fatal("video RAM not yet implemented");
-	}
-	/* switchable RAM bank */
-	if(location < 0xC000)
-	{
-		fatal("RAM bank switching not yet implemented");
-	}
-	/* internal RAM */
-	if(location < 0xE000)
-	{
-		return memory[location];
-	}
+	return memory[location];
+}
+
+uint8_t rom_bank_read(uint16_t location)
+{
+	fatal("bank switching not yet implemented");
+	/* NOTREACHED */
+}
+
+uint8_t not_impl(uint16_t location)
+{
+	fatal("reading from %04X is not yet implemented", location);
+	/* NOTREACHED */
+}
+
+uint8_t ram_bank_read(uint16_t location)
+{
+	fatal("RAM bank switching not yet implemented");
+	/* NOTREACHED */
+}
+
+uint8_t shadow_read(uint16_t location)
+{
+	return memory[location - 0x2000];
+}
+
+uint8_t f_read(uint16_t location)
+{
 	/* who knows?  the SHADOW knows! */
 	if(location < 0xFE00)
 	{
-		return memory[location - 0x2000];
+		return shadow_read(location);
 	}
 	/* OAM */
 	if(location < 0xFEA0)
@@ -47,8 +56,36 @@ uint8_t mem_read8(uint16_t location)
 	{
 		fatal("invalid memory read at %04X", location);
 	}
-	/* I/O / hardware stuff */
 	fatal("still not sure how I want to implement I/O access");
+}
+
+mem_read_fn readers[0x10] = {
+	/* ROM bank #0 - 0x0000..0x3FFF */
+	direct_read, direct_read, direct_read, direct_read,
+	/* switchable bank - 0x4000..0x7FFF */
+	rom_bank_read, rom_bank_read, rom_bank_read, rom_bank_read,
+	/* video memory - 0x8000..0x9FFF */
+	not_impl, not_impl,
+	/* switchable RAM bank - 0xA000..0xBFFF */
+	ram_bank_read, ram_bank_read,
+	/* internal RAM - 0xC000..0xDFFF */
+	direct_read, direct_read,
+	/* who knows? the SHADOW knows! - 0xE000..0xFDFF */
+	shadow_read,
+	/* special stuff - 0xFE00-0xFFFF */
+	f_read
+};
+
+uint8_t mem_read8(uint16_t location)
+{
+	char reader = location >> 12;
+	
+	if(location > 0xFFFF)
+	{
+		fatal("invalid memory access - out of bounds");
+	}
+	
+	return readers[reader](location);
 }
 
 uint16_t mem_read16(uint16_t location)
@@ -56,6 +93,12 @@ uint16_t mem_read16(uint16_t location)
 	return (mem_read8(location+1) << 8) | mem_read8(location);
 }
 
+
+
+
+/***********************************************************************
+ * writers
+ ***********************************************************************/
 void mem_write8(uint16_t location, uint8_t data)
 {
 	if(location < 0xC000 || location >= 0xFE00)
