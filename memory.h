d25c885f9a778572df65faeea22a9274cd1725c6
Elizabeth Myers <elizabeth@interlinked.me>
2014-11-17 07:32:12 -0600

    oops

--- memory.h
+++ memory.h
@@ -14,94 +14,94 @@ typedef enum
 	/* TODO GBC constants */
 
 	/* restart and interrupt vectors */
-	vec_begin = 0x0000;
-	vec_end = 0x00FF;
+	vec_begin = 0x0000,
+	vec_end = 0x00FF,
 
 	/* cartridge data */
-	cart_begin = 0x0100;
+	cart_begin = 0x0100,
 
 	/* 4 bytes */
-	start_instr_begin = 0x0100;
-	start_instr_end = 0x0103;
+	start_instr_begin = 0x0100,
+	start_instr_end = 0x0103,
 
 	/* 10 bytes */
-	game_title_begin = 0x0134;
-	game_title_end = 0x013E;
+	game_title_begin = 0x0134,
+	game_title_end = 0x013E,
 
 	/* 4 bytes */
-	game_desig_begin = 0x013F;
-	game_desig_end = 0x0142;
+	game_desig_begin = 0x013F,
+	game_desig_end = 0x0142,
 
 	/* 1 byte */
-	color_compat = 0x0143;
+	color_compat = 0x0143,
 
 	/* 2 bytes */
-	licensee_begin = 0x0144;
-	licensee_end = 0x0145;
+	licensee_begin = 0x0144,
+	licensee_end = 0x0145,
 
 	/* 1 byte */
-	sgb_compat = 0x0146;
+	sgb_compat = 0x0146,
 
-	cart_type = 0x0147;
+	cart_type = 0x0147,
 
-	cart_rom_size = 0x0148;
+	cart_rom_size = 0x0148,
 
-	cart_ram_size = 0x0149;
+	cart_ram_size = 0x0149,
 
-	cart_dest_code = 0x014A; /* Japanese or not */
+	cart_dest_code = 0x014A, /* Japanese or not */
 
-	old_licensee = 0x014B; /* Mostly unused */
+	old_licensee = 0x014B, /* Mostly unused */
 
-	mask_rom_ver = 0x014C;
+	mask_rom_ver = 0x014C,
 
-	header_checksum = 0x014D;
+	header_checksum = 0x014D,
 
 	/* 2 bytes */
-	cart_checksum_begin = 0x014E;
-	cart_checksum_end = 0x014F;
+	cart_checksum_begin = 0x014E,
+	cart_checksum_end = 0x014F,
 
-	cart_end = 0x014F;
+	cart_end = 0x014F,
 
 	/* Fixed bank */
-	rom0_begin = 0x0150;
-	rom0_end = 0x3FFF;
+	rom0_begin = 0x0150,
+	rom0_end = 0x3FFF,
 
 	/* Switchable bank */
-	rom_bank_begin = 0x4000;
-	rom_bank_end = 0x7FFF;
+	rom_bank_begin = 0x4000,
+	rom_bank_end = 0x7FFF,
 
-	tile_ram_begin = 0x8000;
-	tile_ram_end = 0x97FF;
+	tile_ram_begin = 0x8000,
+	tile_ram_end = 0x97FF,
 
-	bg0_begin = 0x9800;
-	bg0_end = 0x9BFF;
+	bg0_begin = 0x9800,
+	bg0_end = 0x9BFF,
 
-	bg1_begin = 0x9C00;
-	bg1_end = 0x9FFF;
+	bg1_begin = 0x9C00,
+	bg1_end = 0x9FFF,
 
 	/* Not present on all carts! */
-	cart_ram_begin = 0xA000;
-	cart_ram_end = 0xBFFF;
+	cart_ram_begin = 0xA000,
+	cart_ram_end = 0xBFFF,
 
-	ram_begin = 0xC000;
-	ram_end = 0xDFFF;
+	ram_begin = 0xC000,
+	ram_end = 0xDFFF,
 
-	shadow_begin = 0xE000;
-	shadow_end = 0xFDFF;
+	shadow_begin = 0xE000,
+	shadow_end = 0xFDFF,
 
-	oam_begin = 0xFE00;
-	oam_end = 0xFE9F;
+	oam_begin = 0xFE00,
+	oam_end = 0xFE9F,
 
 	/* Contains control registers and other doodads */
-	mmio_begin = 0xFF00;
+	mmio_begin = 0xFF00,
 	/* TODO I/O constants */
-	mmio_end = 0xFF7F;
+	mmio_end = 0xFF7F,
 
-	high_ram_begin = 0xFF80;
-	high_ram_end = 0xFFFE;
+	high_ram_begin = 0xFF80,
+	high_ram_end = 0xFFFE,
 
 	/* Interrupts locked out */
-	int_flag = 0xFFFF;
+	int_flag = 0xFFFF,
 } offsets;
 
 #endif /*!__MEMORY_H_*/
