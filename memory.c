5276eb2656ada7b815852f186a56e8bfec05b938
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2014-11-17 04:49:43 -0600

    yep, this is the most Pentium-iest z80 ever internally

--- memory.c
+++ memory.c
@@ -1,21 +1,67 @@
 #include <stdint.h>	// uint[XX]_t
 
+#include "print.h"	// fatal
+
+
 unsigned char memory[0x10000];
 
 uint8_t mem_read8(uint16_t location)
 {
-	return memory[location];
+	/* ROM bank #0 */
+	if(location < 0x4000)
+	{
+		return memory[location];
+	}
+	/* switchable ROM bank */
+	if(location < 0x8000)
+	{
+		fatal("bank switching not yet implemented");
+	}
+	/* video memory */
+	if(location < 0xA000)
+	{
+		fatal("video RAM not yet implemented");
+	}
+	/* switchable RAM bank */
+	if(location < 0xC000)
+	{
+		fatal("RAM bank switching not yet implemented");
+	}
+	/* internal RAM */
+	if(location < 0xE000)
+	{
+		return memory[location];
+	}
+	/* who knows?  the SHADOW knows! */
+	if(location < 0xFE00)
+	{
+		return memory[location - 0x2000];
+	}
+	/* OAM */
+	if(location < 0xFEA0)
+	{
+		fatal("OAM not yet implemented");
+	}
+	/* invalid */
+	if(location < 0xFF00)
+	{
+		fatal("invalid memory read at %04X", location);
+	}
+	/* I/O / hardware stuff */
+	fatal("still not sure how I want to implement I/O access");
 }
 
 uint16_t mem_read16(uint16_t location)
 {
-	return (memory[location+1] << 8) | memory[location];
+	return (mem_read8(location+1) << 8) | mem_read8(location);
 }
 
 void mem_write8(uint16_t location, uint8_t data)
 {
+	fatal("invalid memory write at %04X", location);
 }
 
 void mem_write16(uint16_t location, uint8_t data)
 {
+	fatal("invalid memory write at %04X", location);
 }
