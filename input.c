69f572a52676ef4fd0680d8527ca242d723a7f1a
Elizabeth Myers <elizabeth@interlinked.me>
2014-11-22 15:22:05 -0600

    Add input! It's probably slow and it's probably wrong but it's something.

--- input.c
+++ input.c
@@ -1,27 +1,45 @@
 #include "config.h"	// macros
-#include <assert.h>	/* assert */
-#include <stdint.h>	/* uint[XX]_t */
+#include <assert.h>	// assert
+#include <stdint.h>	// uint[XX]_t
 
-#include "print.h"	/* error */
-#include "sgherm.h"	/* emu_state */
+#include "ctl_unit.h"	// signal_interrupt
+#include "print.h"	// error
+#include "input.h"	// input_key, defines
+#include "sgherm.h"	// emu_state
 
 
-uint8_t joypad_read(emu_state *restrict state unused, uint16_t reg)
+uint8_t joypad_read(emu_state *restrict state, uint16_t reg)
 {
 	assert(reg == 0xFF00);
-
-	if(reg != 0xFF00)
-	{
-		error("joypad: unknown register %04X (R)", reg);
-		return -1;
-	}
-
-	/* TODO write an input engine, probably with SDL. */
-	return 0;
+	return state->memory[reg];
 }
 
-void joypad_write(emu_state *restrict state unused, uint16_t reg unused, uint8_t data unused)
+void joypad_write(emu_state *restrict state, uint16_t reg, uint8_t data)
 {
-	/* TODO see above */
+	// TODO fake propagation delay and maybe switch bounce
+	state->memory[reg] = data;
 	return;
+}
+
+void joypad_signal(emu_state *restrict state, input_key key, bool down)
+{
+	uint8_t key_mask = mem_read8(state, 0x00F0);
+	uint8_t key_col = key_mask & (RAW_INPUT_P14 | RAW_INPUT_P15);
+
+	if(!(key & key_col))
+	{
+		// Unsolicited key event
+		return;
+	}
+
+	uint8_t report_key = key & ~key_col;
+
+	if(down)
+	{
+		mem_write8(state, 0xFF00, ~report_key);
+	}
+	else
+	{
+		mem_write8(state, 0xFF00, report_key);
+	}
 }
--- input.h
+++ input.h
@@ -6,6 +6,28 @@
 #include <stdint.h>	// uint[XX]_t
 
 
+#define RAW_INPUT_P10	0x1
+#define RAW_INPUT_P11	0x2
+#define RAW_INPUT_P12	0x4
+#define RAW_INPUT_P13	0x8
+
+// Columns set
+#define RAW_INPUT_P14	0x10
+#define RAW_INPUT_P15	0x20
+
+typedef enum
+{
+	INPUT_RIGHT = (RAW_INPUT_P10 | RAW_INPUT_P14),
+	INPUT_LEFT = (RAW_INPUT_P11 | RAW_INPUT_P14),
+	INPUT_UP = (RAW_INPUT_P12 | RAW_INPUT_P14),
+	INPUT_DOWN = (RAW_INPUT_P13 | RAW_INPUT_P14),
+	INPUT_A = (RAW_INPUT_P10 | RAW_INPUT_P15),
+	INPUT_B = (RAW_INPUT_P11 | RAW_INPUT_P15),
+	INPUT_SELECT = (RAW_INPUT_P12 | RAW_INPUT_P15),
+	INPUT_START = (RAW_INPUT_P13 | RAW_INPUT_P15),
+} input_key;
+
+
 uint8_t joypad_read(emu_state *restrict, uint16_t);
 void joypad_write(emu_state *restrict, uint16_t, uint8_t);
 
