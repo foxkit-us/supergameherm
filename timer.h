6d3c7d899b47f4f759a2e76245683a873d1a92a4
Elizabeth Myers <elizabeth@interlinked.me>
2014-11-22 00:10:58 -0600

    Add OS X support to the timer code

--- timer.h
+++ timer.h
@@ -15,7 +15,7 @@ void timer_write(emu_state *restrict, uint16_t, uint8_t);
 void timer_tick(emu_state *restrict);
 
 
-#if defined HAVE_CLOCK_GETTIME
+#ifdef HAVE_CLOCK_GETTIME
 #include <time.h>	// clock_gettime
 
 static inline uint64_t get_time(void)
@@ -30,6 +30,22 @@ static inline uint64_t get_time(void)
 
 	return tv.tv_sec * 1000000000L + tv.tv_nsec;
 }
+#elif defined(HAVE_MACH_CLOCK_H)
+#include <mach/clock.h>	// clock_serv_t, mach_timespec_t, etc.
+#include <mach/mach.h>	// mach_port_deallocate
+
+static inline uint64_t get_time(void)
+{
+	clock_serv_t clock;
+	mach_timespec_t mtv;
+
+	host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &clock);
+	clock_get_time(clock, &mtv);
+	mach_port_deallocate(mach_task_self(), clock);
+
+	return mtv.tv_sec * 1000000000L + mtv.tv_nsec;
+}
+
 #else
 static inline uint64_t get_time(void)
 {
