f728ef12e168ea9eb4573871b529bbcd29c3ec89
Elizabeth Myers <elizabeth@interlinked.me>
2014-11-23 09:19:31 -0600

    Clean up emu_state struct

--- sgherm.h
+++ sgherm.h
@@ -19,125 +19,139 @@ typedef enum
 	CPU_FREQ_GBC = 8388608,
 } cpu_freq;
 
+
+typedef struct _registers
+{
+	/*! The 8/16 bit registers */
+	union
+	{
+		/*! 16-bit registers */
+		struct __16
+		{
+			uint16_t af;
+			uint16_t bc;
+			uint16_t de;
+			uint16_t hl;
+		} _16;
+		/*! 8-bit registers shadowing the 16-bit */
+		struct __8
+		{
+			uint8_t f, a;
+			uint8_t c, b;
+			uint8_t e, d;
+			uint8_t l, h;
+		} _8;
+	} gp;
+
+	uint16_t pc;		/*! Program counter */
+	uint16_t sp;		/*! Stack pointer */
+
+	bool interrupts;	/*! Interrupts enabled */
+} register_map;
+
+typedef struct _lcdc_state
+{
+	uint32_t curr_clk;		/* current clock */
+} lcdc;
+
+typedef struct _ser_state
+{
+	uint16_t curr_clk;		/*! ticks passed */
+	uint8_t in, out;		/*! in / out values */
+	int8_t cur_bit;			/*! the current bit */
+	bool enabled;			/*! transfer active */
+	bool use_internal;		/*! clock source */
+} ser;
+
+typedef struct _snd_state
+{
+	struct _ch1
+	{
+		/*! channel enabled? */
+		bool enabled;
+		/*! this/128Hz = sweep */
+		uint8_t sweep_time;
+		/*! if true, sweep decreases frequency.
+		 *  otherwise, sweep increases frequency. */
+		bool sweep_dec;
+		/*! number of shift */
+		uint8_t shift;
+		/*! wave pattern duty: 1=12.5%,2=25%,3=50%,4=75% */
+		uint8_t wave_duty;
+		/*! sound length */
+		uint8_t length;
+		/*! initial envelope volume */
+		uint8_t envelope_volume;
+		/*! if true, envelope amplifies.
+		 *  otherwise, envelope attenuates. */
+		bool envelope_amp;
+		/*! number of sweeps (0 = stop) */
+		uint8_t sweep;
+		/*! if true, one-shot.  otherwise, loop */
+		bool counter;
+		/*! 11-bit frequency (higher 5 = nothing) */
+		uint16_t frequency;
+	} ch1;
+	struct _ch2
+	{
+		bool enabled;		/*! channel enabled? */
+	} ch2;
+	struct _ch3
+	{
+		bool enabled;		/*! channel enabled? */
+		uint8_t wave[16];	/*! waveform data */
+	} ch3;
+	struct _ch4
+	{
+		bool enabled;		/*! channel enabled? */
+	} ch4;
+	bool enabled;			/*! sound active? */
+} snd;
+
+typedef struct _tm_state
+{
+	uint8_t div;			/*! DIV register */
+	uint8_t tima;			/*! TIMA register */
+	uint8_t rounds;			/*! TMA register */
+	uint16_t ticks_per_tima;	/*! ticks per TIMA++ */
+	uint8_t curr_clk;		/*! ticks passed */
+	bool enabled;			/*! timer armed */
+} timer;
+
+typedef struct _input_state
+{
+	uint8_t col_state;		/*! P14 and P15 */
+	uint8_t row_state;		/*! P10 through P13 */
+} input;
+
+/*! The main emulation state structure */
 typedef struct _emu_state
 {
-	unsigned char memory[MEM_SIZE];		/*! RAM */
-	unsigned char *cart_data;		/*! Cartridge data */
+	unsigned char memory[MEM_SIZE];	/*! RAM */
+	unsigned char *cart_data;	/*! Cartridge data */
 
-	struct _registers
-	{
-		union
-		{
-			struct __16
-			{
-				uint16_t af;
-				uint16_t bc;
-				uint16_t de;
-				uint16_t hl;
-			} _16;
-			struct __8
-			{
-				uint8_t f, a;
-				uint8_t c, b;
-				uint8_t e, d;
-				uint8_t l, h;
-			} _8;
-		} gp;
+	register_map registers;		/*! Registers */
 
-		uint16_t pc;
-		uint16_t sp;
+	bool halt;			/*! waiting for interrupt */
+	bool stop;			/*! deep sleep state (disable LCDC) */
 
-		bool interrupts;		/*! Interrupts enabled */
-	} registers;
+	uint16_t dma_membar_wait;	/*! Clocks left on DMA membar */
 
-	bool halt;				/*! waiting for interrupt */
-	bool stop;				/*! deep sleep state (disable LCDC) */
+	unsigned int wait;		/*! number of clocks */
 
-	uint16_t dma_membar_wait;		/*! Clocks left on DMA membar */
+	uint8_t bank;			/*! current ROM bank */
+	uint8_t ram_bank;		/*! current RAM bank */
 
-	unsigned int wait;			/*! number of clocks */
+	uint64_t cycles;		/*! Present cycle count */
+	uint64_t start_time;		/*! Time started */
+	cpu_freq freq;			/*! CPU frequency */
 
-	uint8_t bank;				/*! current ROM bank */
-	uint8_t ram_bank;			/*! current RAM bank */
-
-	uint64_t cycles;			/*! Present cycle count */
-	uint64_t start_time;			/*! Time started */
-	cpu_freq freq;				/*! CPU frequency */
-
-	struct _lcdc_state
-	{
-		uint32_t curr_clk;		/* current clock */
-	} lcdc_state;
-
-	struct _ser_state
-	{
-		uint16_t curr_clk;		/*! ticks passed */
-		uint8_t in, out;		/*! in / out values */
-		int8_t cur_bit;			/*! the current bit */
-		bool enabled;			/*! transfer active */
-		bool use_internal;		/*! clock source */
-	} ser_state;
-
-	struct _snd_state
-	{
-		struct _ch1
-		{
-			/*! channel enabled? */
-			bool enabled;
-			/*! this/128Hz = sweep */
-			uint8_t sweep_time;
-			/*! if true, sweep decreases frequency.
-			 *  otherwise, sweep increases frequency. */
-			bool sweep_dec;
-			/*! number of shift */
-			uint8_t shift;
-			/*! wave pattern duty: 1=12.5%,2=25%,3=50%,4=75% */
-			uint8_t wave_duty;
-			/*! sound length */
-			uint8_t length;
-			/*! initial envelope volume */
-			uint8_t envelope_volume;
-			/*! if true, envelope amplifies.
-			 *  otherwise, envelope attenuates. */
-			bool envelope_amp;
-			/*! number of sweeps (0 = stop) */
-			uint8_t sweep;
-			/*! if true, one-shot.  otherwise, loop */
-			bool counter;
-			/*! 11-bit frequency (higher 5 = nothing) */
-			uint16_t frequency;
-		} ch1;
-		struct _ch2
-		{
-			bool enabled;		/*! channel enabled? */
-		} ch2;
-		struct _ch3
-		{
-			bool enabled;		/*! channel enabled? */
-			uint8_t wave[16];	/*! waveform data */
-		} ch3;
-		struct _ch4
-		{
-			bool enabled;		/*! channel enabled? */
-		} ch4;
-		bool enabled;			/*! sound active? */
-	} snd_state;
-
-	struct _tm_state
-	{
-		uint8_t div;			/*! DIV register */
-		uint8_t tima;			/*! TIMA register */
-		uint8_t rounds;			/*! TMA register */
-		uint16_t ticks_per_tima;	/*! ticks per TIMA++ */
-		uint8_t curr_clk;		/*! ticks passed */
-		bool enabled;			/*! timer armed */
-	} timer_state;
-
-	struct _input_state
-	{
-		uint8_t col_state;		/*! P14 and P15 */
-		uint8_t row_state;		/*! P10 through P13 */
-	} input_state;
+	// hardware
+	lcdc lcdc_state;
+	snd snd_state;
+	timer timer_state;
+	input input_state;
+	ser ser_state;
 } emu_state;
 
 
