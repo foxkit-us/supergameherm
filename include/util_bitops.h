dec3d4d7875276185cac7662689e4a1beb4b6fed
Elizabeth Myers <elizabeth@interlinked.me>
2014-11-28 14:56:49 -0600

    Quick fix for error in include

--- include/util_bitops.h
+++ include/util_bitops.h
@@ -8,7 +8,7 @@
 #elif HAVE_SYS_ENDIAN_H
 #	include <sys/endian.h>		// see endian.h (but for BSD)
 #else
-#	include "platform/util_swap.h"	// Rolling our own :(
+#	include "platform/swap.h"	// Rolling our own :(
 #endif
 
 
