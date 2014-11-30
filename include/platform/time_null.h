d808e99ac0da26d92cbfecf00d867822deaeabf9
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2014-11-29 18:22:38 -0600

    Least Common Denominator Programming™

--- include/platform/time_null.h
+++ include/platform/time_null.h
@@ -3,9 +3,13 @@
 
 #include "config.h"	// macros, bool, uint[XX]_t
 
+#include <time.h>
+
 static inline uint64_t get_time(void)
 {
-	return 0;
+	clock_t res = clock();
+	uint64_t val = res * (1000000000L / CLOCKS_PER_SEC);
+	return val;
 }
 
 #endif /*__TIME_NULL_H__*/
