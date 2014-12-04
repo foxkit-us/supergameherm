94b0970907c8604bf89a2f4f69252721b62090a3
Elizabeth Myers <elizabeth@interlinked.me>
2014-12-03 22:16:36 -0600

    Make LCDC endian-agnostic

--- include/lcdc.h
+++ include/lcdc.h
@@ -12,22 +12,39 @@ struct oam_t
 	uint8_t chr;	/*! CHR code */
 	struct
 	{
+#ifdef LITTLE_ENDIAN
 		bool priority:1;		/*! Priority to obj/bg */
 		bool vflip:1;			/*! Vertical flip flag */
 		bool hflip:1;			/*! Horizontal flip */
-		unsigned int pal_dmg:1;		/*! Palette selectin (DMG only) */
+		unsigned int pal_dmg:1;		/*! Palette selection (DMG only) */
 		unsigned int char_bank:1;	/*! Character bank (CGB only) */
 		unsigned int pal_cgb:3;		/*! Palette selection CGB only) */
+#else
+		unsigned int pal_cgb:3;		/*! Palette selection CGB only) */
+		unsigned int char_bank:1;	/*! Character bank (CGB only) */
+		unsigned int pal_dmg:1;		/*! Palette selection (DMG only) */
+		bool hflip:1;			/*! Horizontal flip */
+		bool vflip:1;			/*! Vertical flip flag */
+		bool priority:1;		/*! Priority to obj/bg */
+#endif
 	} flags;
 };
 
 struct cps_t
 {
+#ifdef LITTLE_ENDIAN
 	bool pal_sel:1;			/*! Palette changes on next write */
 	unsigned int notused:1;		/*! Not used */
 	unsigned int pal_num:3;		/*! Select palette number */
 	unsigned int pal_data_num:2;	/*! Select palette data number */
 	bool hl:1;			/*! Specify H/L */
+#else
+	bool hl:1;			/*! Specify H/L */
+	unsigned int pal_data_num:2;	/*! Select palette data number */
+	unsigned int pal_num:3;		/*! Select palette number */
+	unsigned int notused:1;		/*! Not used */
+	bool pal_sel:1;			/*! Palette changes on next write */
+#endif
 };
 
 struct lcdc_state_t
@@ -46,6 +63,7 @@ struct lcdc_state_t
 		uint8_t reg;
 		struct
 		{
+#ifdef LITTLE_ENDIAN
 			bool dmg_bg:1;		/*! BG is on or off (DMG only) */
 			bool obj:1;		/*! OBJ's are off or on */
 			bool obj_block_size:1;	/*! Size of OBJ's are 8x8 or 8x16 */
@@ -54,6 +72,16 @@ struct lcdc_state_t
 			bool win:1;		/*! Windowing on */
 			bool win_code_sel:1;	/*! Active window at 9800-9BFF or 9C00-9FFF */
 			bool enable:1;		/*! LCD enabled */
+#else
+			bool enable:1;		/*! LCD enabled */
+			bool win_code_sel:1;	/*! Active window at 9800-9BFF or 9C00-9FFF */
+			bool win:1;		/*! Windowing on */
+			bool bg_char_sel:1;	/*! BG character data at 8800-97FF or 8000-8FFF */
+			bool bg_code_sel:1;	/*! BG code data at 9800-9BFF or 9C00-9FFF */
+			bool obj_block_size:1;	/*! Size of OBJ's are 8x8 or 8x16 */
+			bool obj:1;		/*! OBJ's are off or on */
+			bool dmg_bg:1;		/*! BG is on or off (DMG only) */
+#endif
 		} params;
 	} lcd_control;
 
@@ -62,6 +90,7 @@ struct lcdc_state_t
 		uint8_t reg;
 		struct
 		{
+#ifdef LITTLE_ENDIAN
 			/*! Mode flag
 			 * Mode 00: enable CPU access to VRAM
 			 * Mode 01: V-Blank interval
@@ -76,12 +105,28 @@ struct lcdc_state_t
 			bool mode_10:1;		/*! int on mode 10 selection */
 			bool lyc:1;		/*! int on LY matching selection */
 			unsigned int notused:1;	/*! Upper bit padding */
+#else
+			unsigned int notused:1;	/*! Upper bit padding */
+			bool lyc:1;		/*! int on LY matching selection */
+			bool mode_10:1;		/*! int on mode 10 selection */
+			bool mode_01:1;		/*! int on mode 01 selection */
+			bool mode_00:1;		/*! int on mode 00 selection */
+			bool lyc_state:1;	/*! LYC matches LY */
+
+			/*! Mode flag
+			 * Mode 00: enable CPU access to VRAM
+			 * Mode 01: V-Blank interval
+			 * Mode 10: OAM search
+			 * Mode 11: LCD transfer
+			 */
+			unsigned int mode_flag:2;
+#endif
 		} params;
 	} stat;
 
 	union
 	{
-		/*! CGB only */
+		/*! CGB only (FIXME palette RAM) */
 		struct
 		{
 			cps bcps;
@@ -96,18 +141,32 @@ struct lcdc_state_t
 		{
 			struct
 			{
+#ifdef LITTLE_ENDIAN
+				unsigned int shade_4:2;
+				unsigned int shade_3:2;
+				unsigned int shade_2:2;
+				unsigned int shade_1:2;
+#else
 				unsigned int shade_1:2;
 				unsigned int shade_2:2;
 				unsigned int shade_3:2;
 				unsigned int shade_4:2;
+#endif
 			} dmg_pal;
 
 			struct
 			{
+#ifdef LITTLE_ENDIAN
+				unsigned int notused:2;
+				unsigned int shade_3:2;
+				unsigned int shade_2:2;
+				unsigned int shade_1:2;
+#else
 				unsigned int shade_1:2;
 				unsigned int shade_2:2;
 				unsigned int shade_3:2;
 				unsigned int notused:2;
+#endif
 			} obp[2];
 		};
 	};
