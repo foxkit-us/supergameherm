846523fe160dc9547d7db9fcb6f1dcf5cd9be4f4
Elizabeth Myers <elizabeth@interlinked.me>
2014-12-06 01:29:02 -0600

    I forgot this >:|

---
+++ src/frontend.c
@@ -0,0 +1,42 @@
+#include "config.h"	// bool
+#include "frontend.h"	// frontend
+
+/*! input frontends */
+const frontend_input *frontend_set_input[] =
+{
+	&null_frontend_input,
+#ifdef HAVE_LIBCACA
+	&libcaca_frontend_input,
+#else
+	&null_frontend_input,
+#endif
+};
+
+/*! Video frontends */
+const frontend_video *frontend_set_video[] =
+{
+	&null_frontend_video,
+#ifdef HAVE_LIBCACA
+	&libcaca_frontend_video,
+#else
+	&null_frontend_video,
+#endif
+};
+
+/*! Audio frontends */
+const frontend_audio *frontend_set_audio[] =
+{
+	&null_frontend_audio,
+	&null_frontend_audio,
+};
+
+/*! Event loops */
+const frontend_event_loop frontend_set_event_loop[] =
+{
+	&null_event_loop,
+#ifdef HAVE_LIBCACA
+	&libcaca_event_loop,
+#else
+	&null_event_loop,
+#endif
+};
