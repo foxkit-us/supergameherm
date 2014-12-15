e410b55906ebb8093ead2758c2dffb82dfa88d29
Elizabeth Myers <elizabeth@interlinked.me>
2014-12-14 20:21:16 -0600

    It's the same on all platforms

--- src/sdl2_frontend.c
+++ src/sdl2_frontend.c
@@ -21,17 +21,10 @@
 #define BPP 32
 #define PITCH 4
 
-#ifdef LITTLE_ENDIAN
 #define RED	0x000000ff
 #define GREEN	0x0000ff00
 #define BLUE	0x00ff0000
 #define ALPHA	0xff000000
-#else
-#define RED	0xff000000
-#define GREEN	0x00ff0000
-#define BLUE	0x0000ff00
-#define ALPHA	0x000000ff
-#endif
 
 
 typedef struct sdl2_video_data_t
