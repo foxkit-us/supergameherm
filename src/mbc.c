4821fa9f75ef314f01800f1b3050743071df7dc8
Elizabeth Myers <elizabeth@interlinked.me>
2014-12-23 00:53:34 -0600

    Implement forgotten op in MBC3

--- src/mbc.c
+++ src/mbc.c
@@ -404,6 +404,9 @@ static inline void mbc3_write(emu_state *restrict state, uint16_t location, uint
 		// Bank switch
 		state->mbc.rom_bank = value & 0x7F;
 		break;
+	case 0x4:
+		state->mbc.mbc3.rtc_select = value;
+		break;
 	case 0xA:
 	case 0xB:
 		// Select RTC or RAM bank
