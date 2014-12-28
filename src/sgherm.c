b9e3f96e399640e0a7d296313e234d3ed12c5603
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2014-12-27 19:14:07 -0600

    Fix build when throttling vblank is enabled

--- src/sgherm.c
+++ src/sgherm.c
@@ -81,7 +81,7 @@ bool step_emulator(emu_state *restrict state)
 
 #ifdef THROTTLE_VBLANK
 	// Wait for vblank
-	if(unlikely(state->lcdc.stat.mode_flag == 1 &&
+	if(unlikely(LCDC_STAT_MODE_FLAG(state) == 1 &&
 		state->lcdc.curr_clk == 0))
 	{
 		uint64_t t = get_time();
