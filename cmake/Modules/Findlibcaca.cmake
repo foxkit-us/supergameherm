3ad1f31b690d1a944155b2af5f6d72e4fb67a269
Elizabeth Myers <elizabeth@interlinked.me>
2014-12-31 11:37:19 -0600

    Improve libcaca detection

--- cmake/Modules/Findlibcaca.cmake
+++ cmake/Modules/Findlibcaca.cmake
@@ -2,18 +2,12 @@
 
 find_path(libcaca_INCLUDE_DIR caca.h)
 find_library(libcaca_LIBRARY caca)
+check_symbol_exists(CACA_API_VERSION_1 caca.h CACA_API_VERSION_1)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libcaca DEFAULT_MSG libcaca_LIBRARY
-	libcaca_INCLUDE_DIR)
 
-if(FOUND_libcaca)
-	check_symbol_exists(CACA_API_VERSION_1 caca.h CACA_API_VERSION_1)
-	if(not defined CACA_API_VERSION_1)
-		message("Your libcaca is too new (nobody ships 1.0 yet :/)")
-		unset(libcaca_INCLUDE_DIR)
-		unset(libcaca_LIBRARY)
-	endif()
-endif()
+find_package_handle_standard_args(libcaca FOUND_VAR libcaca_FOUND
+	REQUIRED_VARS libcaca_LIBRARY libcaca_INCLUDE_DIR
+	VERSION_VAR CACA_API_VERSION_1)
 
 mark_as_advanced(libcaca_INCLUDE_DIR libcaca_LIBRARY)
