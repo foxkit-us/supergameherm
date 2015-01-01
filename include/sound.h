99d84f56f916a8f6e32b2b7bb47ddb8cf90c4786
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2015-01-02 10:06:30 +1300

    preliminary ch3/ch4 support

--- include/sound.h
+++ include/sound.h
@@ -13,62 +13,82 @@ struct snd_state_t
 
 	struct _ch1
 	{
-		bool enabled; //! channel enabled?
-		uint8_t sweep_time; //! this/128Hz = sweep
+		bool enabled;		//! channel enabled?
+		bool initial;		//! channel started?
+		uint8_t sweep_time;	//! this/128Hz = sweep
 		/*! if true, sweep increases period.
 		 *  otherwise, sweep decreases period. */
 		bool sweep_dec;
-		uint8_t sweep_shift; //! number of shift
-		uint8_t wave_duty; //! wave pattern duty: 1=12.5%,2=25%,3=50%,4=75%
-		uint8_t length; //! sound length
-		int8_t envelope_volume; //! initial envelope volume
-		/*! if true, envelope amplifies.
-		 *  otherwise, envelope attenuates. */
-		bool envelope_amp;
-		uint8_t envelope_speed; //! speed of envelope
-		bool counter; //! if true, one-shot.  otherwise, loop
-		uint16_t period; //! 11-bit period (higher 5 = nothing)
-		bool s01; //! output to S01
-		bool s02; //! output to S02
+		uint8_t sweep_shift;	//! number of shift
+		uint8_t wave_duty;	//! wave pattern duty: 1=12.5%,2=25%,3=50%,4=75%
+		uint8_t length;		//! sound length
+		int8_t envelope_volume;	//! initial envelope volume
+		bool envelope_amp;	//! determines if envelope amplifies (c.f. attenuates)
+		uint8_t envelope_speed;	//! speed of envelope
+		bool counter;		//! if true, one-shot.  otherwise, loop
+		uint16_t period;	//! 11-bit period (higher 5 = nothing)
+		bool s01;		//! output to S01
+		bool s02;		//! output to S02
 
 		// Audio runtime values
-		uint16_t per_remain; //! Counter for period
-		uint8_t outseq; //! Output sequence number for wave duty
+		uint16_t per_remain;	//! Counter for period
+		uint8_t outseq;		//! Output sequence number for wave duty
 	} ch1;
 
 	struct _ch2
 	{
-		bool enabled; //! channel enabled?
-		uint8_t wave_duty; //! wave pattern duty: 1=12.5%,2=25%,3=50%,4=75%
-		uint8_t length; //! sound length
-		int8_t envelope_volume; //! initial envelope volume
-		/*! if true, envelope amplifies.
-		 *  otherwise, envelope attenuates. */
-		bool envelope_amp;
-		uint8_t envelope_speed; //! speed of envelope
-		bool counter; //! if true, one-shot.  otherwise, loop
-		uint16_t period; //! 11-bit period (higher 5 = nothing)
-		bool s01; //! output to S01
-		bool s02; //! output to S02
+		bool enabled;		//! channel enabled?
+		bool initial;		//! channel started?
+		uint8_t wave_duty;	//! wave pattern duty: 1=12.5%,2=25%,3=50%,4=75%
+		uint8_t length;		//! sound length
+		int8_t envelope_volume;	//! initial envelope volume
+		bool envelope_amp;	//! determines if envelope amplifies (c.f. attenuates)
+		uint8_t envelope_speed;	//! speed of envelope
+		bool counter;		//! if true, one-shot.  otherwise, loop
+		uint16_t period;	//! 11-bit period (higher 5 = nothing)
+		bool s01;		//! output to S01
+		bool s02;		//! output to S02
 
 		// Audio runtime values
-		uint16_t per_remain; //! Counter for period
-		uint8_t outseq; //! Output sequence number for wave duty
+		uint16_t per_remain;	//! Counter for period
+		uint8_t outseq;		//! Output sequence number for wave duty
 	} ch2;
 
 	struct _ch3
 	{
 		bool enabled;		//! channel enabled?
+		bool initial;		//! channel started?
+		bool counter;		//! if true, one-shot.  otherwise, loop
+		uint8_t length;		//! sound length
+		uint8_t volume;		//! sound volume value
+		uint16_t period;	//! 11-bit period (higher 5 = nothing)
 		uint8_t wave[16];	//! waveform data
 		bool s01;		//! output to S01
 		bool s02;		//! output to S02
+
+		// Audio runtime values
+		uint16_t per_remain;	//! Counter for period
+		uint8_t outseq;		//! Output sequence number for waveform
 	} ch3;
 
 	struct _ch4
 	{
 		bool enabled;		//! channel enabled?
+		bool initial;		//! channel started?
+		uint8_t length;		//! sound length
+		int8_t envelope_volume;	//! initial envelope volume
+		bool envelope_amp;	//! determines if envelope amplifies (c.f. attenuates)
+		uint8_t envelope_speed;	//! speed of envelope
+		uint8_t period_exp;	//! exponential part of period
+		uint8_t period_mul;	//! (mostly) linear part of period
+		bool is_short;		//! determines if LFSR is 7-bit (c.f. 15-bit)
+		bool counter;		//! if true, one-shot.  otherwise, loop
 		bool s01;		//! output to S01
 		bool s02;		//! output to S02
+
+		// Audio runtime values
+		int32_t per_remain;	//! Counter for period
+		uint16_t lfsr;		//! LFSR counter data
 	} ch4;
 
 	bool enabled;			//! sound active?
--- src/sound.c
+++ src/sound.c
@@ -39,6 +39,7 @@ uint8_t sound_read(emu_state *restrict state, uint16_t reg)
 	{
 		return state->snd.ch1.counter << 6;
 	}
+
 	//! NR 21 - ch 2 - wave pattern duty
 	case 0xFF16:
 	{
@@ -119,11 +120,11 @@ void sound_write(emu_state *restrict state, uint16_t reg, uint8_t data)
 	case 0xFF10:
 	{
 		state->snd.ch1.sweep_time = (data >> 4) & 0x7;
-		state->snd.ch1.sweep_dec = ((data & 0x08) == 0x08);
+		state->snd.ch1.sweep_dec = ((data & 0x08) != 0);
 		state->snd.ch1.sweep_shift = (data & 0x7);
 		break;
 	}
-	//! NR 11 - ch 1 - legnth/duty
+	//! NR 11 - ch 1 - length/duty
 	case 0xFF11:
 	{
 		state->snd.ch1.length = (data & 0x3F);
@@ -134,7 +135,7 @@ void sound_write(emu_state *restrict state, uint16_t reg, uint8_t data)
 	case 0xFF12:
 	{
 		state->snd.ch1.envelope_volume = (data >> 4);
-		state->snd.ch1.envelope_amp = ((data & 0x08) == 0x08);
+		state->snd.ch1.envelope_amp = ((data & 0x08) != 0);
 		state->snd.ch1.envelope_speed = (data & 0x7);
 		break;
 	}
@@ -152,10 +153,12 @@ void sound_write(emu_state *restrict state, uint16_t reg, uint8_t data)
 		uint16_t val = state->snd.ch1.period & 0x00FF;
 		val |= (data & 0x7) << 8;
 		state->snd.ch1.period = val;
-		state->snd.ch1.counter = ((data & 0x40) == 0x40);
+		state->snd.ch1.initial = ((data & 0x80) != 0);
+		state->snd.ch1.counter = ((data & 0x40) != 0);
 		break;
 	}
-	//! NR 21 - ch 2 - legnth/duty
+
+	//! NR 21 - ch 2 - length/duty
 	case 0xFF16:
 	{
 		state->snd.ch2.length = (data & 0x3F);
@@ -166,7 +169,7 @@ void sound_write(emu_state *restrict state, uint16_t reg, uint8_t data)
 	case 0xFF17:
 	{
 		state->snd.ch2.envelope_volume = (data >> 4);
-		state->snd.ch2.envelope_amp = ((data & 0x08) == 0x08);
+		state->snd.ch2.envelope_amp = ((data & 0x08) != 0);
 		state->snd.ch2.envelope_speed = (data & 0x7);
 		break;
 	}
@@ -184,14 +187,83 @@ void sound_write(emu_state *restrict state, uint16_t reg, uint8_t data)
 		uint16_t val = state->snd.ch2.period & 0x00FF;
 		val |= (data & 0x7) << 8;
 		state->snd.ch2.period = val;
-		state->snd.ch2.counter = ((data & 0x40) == 0x40);
+		state->snd.ch2.initial = ((data & 0x80) != 0);
+		state->snd.ch2.counter = ((data & 0x40) != 0);
 		break;
 	}
+
+	//! NR 30 - ch 3 - enable
+	case 0xFF1A:
+	{
+		state->snd.ch3.enabled = (data & 0x80) != 0;
+		break;
+	}
+	//! NR 31 - ch 3 - length
+	case 0xFF1B:
+	{
+		state->snd.ch3.length = data;
+		break;
+	}
+	//! NR 32 - ch 3 - volume
+	case 0xFF1C:
+	{
+		state->snd.ch3.volume = ((data >> 5) & 0x03);
+		break;
+	}
+	//! NR 33 - ch 3 - period LSB
+	case 0xFF1D:
+	{
+		uint16_t val = state->snd.ch3.period & 0xFF00;
+		val |= data;
+		state->snd.ch3.period = val;
+		break;
+	}
+	//! NR 34 - ch 3 - misc
+	case 0xFF1E:
+	{
+		uint16_t val = state->snd.ch3.period & 0x00FF;
+		val |= (data & 0x7) << 8;
+		state->snd.ch3.period = val;
+		state->snd.ch3.initial = ((data & 0x80) != 0);
+		state->snd.ch3.counter = ((data & 0x40) != 0);
+		break;
+	}
+
+	//! NR 41 - ch 4 - length
+	case 0xFF20:
+	{
+		state->snd.ch4.length = (data & 0x3F);
+		break;
+	}
+	//! NR 42 - ch 4 - envelope
+	case 0xFF21:
+	{
+		state->snd.ch4.envelope_volume = (data >> 4);
+		state->snd.ch4.envelope_amp = ((data & 0x08) != 0);
+		state->snd.ch4.envelope_speed = (data & 0x7);
+		break;
+	}
+	//! NR 43 - ch 4 - period/LFSR
+	case 0xFF22:
+	{
+		state->snd.ch4.period_exp = (data >> 4);
+		state->snd.ch4.is_short = ((data & 0x08) != 0);
+		state->snd.ch4.period_mul = (data & 0x07);
+		break;
+	}
+	//! NR 44 - ch 4 - misc
+	case 0xFF23:
+	{
+		state->snd.ch4.initial = ((data & 0x80) != 0);
+		state->snd.ch4.counter = ((data & 0x40) != 0);
+		break;
+	}
+
 	//! NR 50 - ch control
 	case 0xFF24:
 	{
-		state->snd.s02 = ((data & 0x80) == 0x80);
-		state->snd.s01 = ((data & 0x08) == 0x08);
+		state->snd.s02 = ((data & 0x80) != 0);
+		state->snd.s01 = ((data & 0x08) != 0);
 		state->snd.s02_volume = ((data & 0x70) >> 4);
 		state->snd.s01_volume = (data & 0x07);
 		break;
@@ -199,20 +271,20 @@ void sound_write(emu_state *restrict state, uint16_t reg, uint8_t data)
 	//! NR 51 - 'stereo' emulation
 	case 0xFF25:
 	{
-		state->snd.ch1.s01 = ((data & 0x01) == 0x01);
-		state->snd.ch2.s01 = ((data & 0x02) == 0x02);
-		state->snd.ch3.s01 = ((data & 0x04) == 0x04);
-		state->snd.ch4.s01 = ((data & 0x08) == 0x08);
-		state->snd.ch1.s02 = ((data & 0x10) == 0x10);
-		state->snd.ch2.s02 = ((data & 0x20) == 0x20);
-		state->snd.ch3.s02 = ((data & 0x40) == 0x40);
-		state->snd.ch4.s02 = ((data & 0x80) == 0x80);
+		state->snd.ch1.s01 = ((data & 0x01) != 0);
+		state->snd.ch2.s01 = ((data & 0x02) != 0);
+		state->snd.ch3.s01 = ((data & 0x04) != 0);
+		state->snd.ch4.s01 = ((data & 0x08) != 0);
+		state->snd.ch1.s02 = ((data & 0x10) != 0);
+		state->snd.ch2.s02 = ((data & 0x20) != 0);
+		state->snd.ch3.s02 = ((data & 0x40) != 0);
+		state->snd.ch4.s02 = ((data & 0x80) != 0);
 		break;
 	}
 	//! NR 52 - sound enable
 	case 0xFF26:
 	{
-		state->snd.enabled = ((data & 0x80) == 0x80);
+		state->snd.enabled = ((data & 0x80) != 0);
 		break;
 	}
 	default:
@@ -228,6 +300,20 @@ void sound_fetch_s16ne(emu_state *restrict state, int16_t *restrict outbuf, size
 	size_t i;
 
 	snd_state *snd = &state->snd;
+
+	int ch1_period = snd->ch1.period - 0x0800; 
+	int ch2_period = snd->ch2.period - 0x0800; 
+	int ch3_period = snd->ch3.period - 0x0800; 
+	int ch4_period = ((snd->ch4.period_mul == 0
+			? 1
+			: (snd->ch4.period_mul<<1))
+		<<(snd->ch4.period_exp+0));
+
+	if(snd->ch4.lfsr == 0) snd->ch4.lfsr = 0xFFFF;
+	uint16_t lfsr_tap = (snd->ch4.is_short
+		? 0x4040
+		: 0x4000);
+	//printf("per %c %i\n", snd->ch4.is_short?'s':'L', ch4_period);
 
 	for(i = 0; i < len_samples; i++)
 	{
@@ -257,18 +343,28 @@ void sound_fetch_s16ne(emu_state *restrict state, int16_t *restrict outbuf, size
 			else snd->ch2.envelope_volume -= snd->ch2.envelope_speed;
 			if(snd->ch2.envelope_volume < 0) snd->ch2.envelope_volume = 0;
 			else if(snd->ch2.envelope_volume > 15) snd->ch2.envelope_volume = 15;
+
+			// CH3 has no env
+
+			// CH4 env
+			if(snd->ch4.envelope_amp) snd->ch4.envelope_volume += snd->ch4.envelope_speed;
+			else snd->ch4.envelope_volume -= snd->ch4.envelope_speed;
+			if(snd->ch4.envelope_volume < 0) snd->ch4.envelope_volume = 0;
+			else if(snd->ch4.envelope_volume > 15) snd->ch4.envelope_volume = 15;
 		}
 
 		// TODO: shift some of this stuff out
 		// TODO: once we've done that, make this more accurate
 		// Update period
-		if(1 || snd->ch1.enabled) snd->ch1.per_remain += pinc;
-		if(1 || snd->ch2.enabled) snd->ch2.per_remain += pinc;
+		if(snd->ch1.initial) snd->ch1.per_remain += pinc;
+		if(snd->ch2.initial) snd->ch2.per_remain += pinc;
+		if(snd->ch3.enabled && snd->ch3.initial) snd->ch3.per_remain += pinc;
+		if(snd->ch4.initial) snd->ch4.per_remain -= pinc;
 
 		if(snd->ch1.period > 0)
 		while(snd->ch1.per_remain >= 0x0800)
 		{
-			snd->ch1.per_remain += snd->ch1.period - 0x0800;
+			snd->ch1.per_remain += ch1_period;
 			snd->ch1.outseq += 1;
 			snd->ch1.outseq &= 7;
 		}
@@ -276,23 +372,69 @@ void sound_fetch_s16ne(emu_state *restrict state, int16_t *restrict outbuf, size
 		if(snd->ch2.period > 0)
 		while(snd->ch2.per_remain >= 0x0800)
 		{
-			snd->ch2.per_remain += snd->ch2.period - 0x0800;
+			snd->ch2.per_remain += ch2_period;
 			snd->ch2.outseq += 1;
 			snd->ch2.outseq &= 7;
 		}
 
-		// TODO: other channels
+		if(snd->ch3.period > 0)
+		while(snd->ch3.per_remain >= 0x0800)
+		{
+			snd->ch3.per_remain += ch3_period;
+			snd->ch3.outseq += 1;
+			snd->ch3.outseq &= 31;
+		}
 
+		if(ch4_period > 0)
+		while(snd->ch4.per_remain < 0)
+		{
+			snd->ch4.per_remain += ch4_period;
+
+			// Update LFSR
+			int lbit = (snd->ch4.lfsr^(snd->ch4.lfsr>>1))&1;
+
+			snd->ch4.lfsr >>= 1;
+
+			if(lbit != 0)
+			{
+				snd->ch4.lfsr ^= lfsr_tap;
+			}
+		}
+
+		// CH 1
+		if(snd->ch1.initial)
 		if((au_pulses[snd->ch1.wave_duty] >> snd->ch1.outseq) & 1)
 		{
 			if(snd->ch1.s01) vl += snd->ch1.envelope_volume;
 			if(snd->ch1.s02) vr += snd->ch1.envelope_volume;
 		}
 
+		// CH 2
+		if(snd->ch2.initial)
 		if((au_pulses[snd->ch2.wave_duty] >> snd->ch2.outseq) & 1)
 		{
 			if(snd->ch2.s01) vl += snd->ch2.envelope_volume;
 			if(snd->ch2.s02) vr += snd->ch2.envelope_volume;
+		}
+
+		// CH 3
+		if(snd->ch3.enabled && snd->ch3.initial && snd->ch3.volume != 0)
+		{
+			uint8_t v3 = snd->ch3.wave[snd->ch3.outseq>>1];
+			v3 = ((snd->ch3.outseq&1) != 0
+				? v3>>4
+				: v3&0x0F);
+
+			if(snd->ch3.s01) vl += v3 >> (snd->ch3.volume-1);
+			if(snd->ch3.s01) vr += v3 >> (snd->ch3.volume-1);
+		}
+
+		// CH 4
+		if(snd->ch4.initial)
+		if((~snd->ch4.lfsr) & 1)
+		{
+			if(snd->ch4.s01) vl += snd->ch4.envelope_volume;
+			if(snd->ch4.s02) vr += snd->ch4.envelope_volume;
 		}
 
 		// Amplify
