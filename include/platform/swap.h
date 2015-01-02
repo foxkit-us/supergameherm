99ac5307887b190b246bde6aa36071a5a9a34d34
Elizabeth Myers <elizabeth@interlinked.me>
2015-01-02 05:18:21 -0600

    Clean up swap stuff

--- include/platform/swap.h
+++ include/platform/swap.h
@@ -4,7 +4,7 @@
 #include "config.h"	// macros, bool, uint[XX]_t
 
 // Check for BSD or Linux-style interface
-#ifdef HAVE_ENDIAN_H
+#if defined(HAVE_ENDIAN_H)
 #	include <endian.h>	// be??toh
 #elif defined(HAVE_SYS_ENDIAN_H)
 #	include <sys/endian.h>	// see endian.h (but for BSD)
@@ -14,41 +14,43 @@
 #	define le16toh(x) OSSwapLittleToHostInt16(x)
 #	define htobe16(x) OSSwapHostToBigInt16(x)
 #	define htole16(x) OSSwapHostToLittleInt16(x)
-#	define be32toh(x) OSSwapBigToHostInt16(x)
-#	define le32toh(x) OSSwapLittleToHostInt16(x)
+#	define be32toh(x) OSSwapBigToHostInt32(x)
+#	define le32toh(x) OSSwapLittleToHostInt32(x)
 #	define htobe32(x) OSSwapHostToBigInt32(x)
 #	define htole32(x) OSSwapHostToLittleInt32(x)
 #else
 
 // Implement a BSD-like interface as a fallback
+#if defined(HAVE_BSWAP_16)
+#	define SWAP16(n) __bswap_16(n)
+#	define SWAP32(n) __bswap_32(n)
+#elif defined(HAVE_BYTESWAP_USHORT)
+#	define SWAP16(n) _byteswap_ushort(n)
+#	define SWAP32(n) _byteswap_ulong(n)
+#else
+#	define SWAP16(n) (((uint16_t)(n) >> 8) | ((uint16_t)(n) << 8))
+#	define SWAP32(n) (((uint32_t)(n) >> 16) | ((uint32_t)(n) << 16))
+#endif /*defined(HAVE_BSWAP_16) || defined(HAVE_BYTESWAP_USHORT)*/
 
-static inline uint16_t be16toh(uint16_t big_endian_16bits)
+uint16_t be16toh(uint16_t big_endian_16bits)
 {
 #if defined(BIG_ENDIAN)
 	return big_endian_16bits;
-#elif defined(HAVE_BSWAP_16)
-	return __bswap_16(big_endian_16bits);
-#elif defined(HAVE_BYTESWAP_USHORT)
-	return _byteswap_ushort(big_endian_16bits);
 #else
-	return (uint16_t)(big_endian_16bits >> 8) | (uint16_t)(big_endian_16bits << 8);
+	return SWAP16(big_endian_16bits);
 #endif /*BIG_ENDIAN*/
 }
 
-static inline uint16_t le16toh(uint16_t little_endian_16bits)
+uint16_t le16toh(uint16_t little_endian_16bits)
 {
 #if defined(LITTLE_ENDIAN)
 	return little_endian_16bits;
-#elif defined(HAVE_BSWAP_16)
-	return __bswap_16(little_endian_16bits);
-#elif defined(HAVE_BYTESWAP_USHORT)
-	return _byteswap_ushort(little_endian_16bits);
 #else
-	return (uint16_t)(little_endian_16bits >> 8) | (uint16_t)(little_endian_16bits << 8);
+	return SWAP16(little_endian_16bits);
 #endif /*LITTLE_ENDIAN*/
 }
 
-static inline uint16_t htole16(uint16_t host_16bits)
+uint16_t htole16(uint16_t host_16bits)
 {
 #if defined(LITTLE_ENDIAN)
 	return host_16bits;
@@ -57,43 +59,34 @@ static inline uint16_t htole16(uint16_t host_16bits)
 #endif /*LITTLE_ENDIAN*/
 }
 
-static inline uint16_t htobe16(uint16_t host_16bits)
+uint16_t htobe16(uint16_t host_16bits)
 {
 #if defined(BIG_ENDIAN)
 	return host_16bits;
 #else
 	return le16toh(host_16bits);
-#endif
-}
-
-//!
-static inline uint32_t be32toh(uint32_t big_endian_32bits)
-{
-#if defined(BIG_ENDIAN)
-	return big_endian_32bits;
-#elif defined(HAVE_BSWAP_32)
-	return __bswap_32(big_endian_32bits);
-#elif defined(HAVE_BYTESWAP_ULONG)
-	return _byteswap_ulong(big_endian_32bits);
-#else
-	return (((uint32_t)be16toh(big_endian_32bits)) << 16) | (uint32_t)(be16toh(big_endian_32bits >> 16));
 #endif /*BIG_ENDIAN*/
 }
 
-static inline uint32_t le32toh(uint32_t little_endian_32bits)
+uint32_t be32toh(uint32_t big_endian_32bits)
+{
+#if defined(BIG_ENDIAN)
+	return big_endian_32bits;
+#else
+	return SWAP32(big_endian_32bits);
+#endif /*BIG_ENDIAN*/
+}
+
+uint32_t le32toh(uint32_t little_endian_32bits)
 {
 #if defined(LITTLE_ENDIAN)
 	return little_endian_32bits;
-#elif defined(HAVE_BSWAP_32)
-	return __bswap_32(little_endian_32bits);
-#elif defined(HAVE_BYTESWAP_ULONG)
-	return _byteswap_ulong(little_endian_32bits);
 #else
-	return (((uint32_t)le16toh(little_endian_32bits)) << 16) | (uint32_t)(le16toh(little_endian_32bits >> 16));
+	return SWAP32(little_endian_32bits);
 #endif /*LITTLE_ENDIAN*/
 }
 
-static inline uint32_t htole32(uint32_t host_32bits)
+uint32_t htole32(uint32_t host_32bits)
 {
 #if defined(LITTLE_ENDIAN)
 	return host_32bits;
@@ -102,15 +95,15 @@ static inline uint32_t htole32(uint32_t host_32bits)
 #endif /*LITTLE_ENDIAN*/
 }
 
-static inline uint32_t htobe32(uint32_t host_32bits)
+uint32_t htobe32(uint32_t host_32bits)
 {
 #if defined(BIG_ENDIAN)
 	return host_32bits;
 #else
 	return le32toh(host_32bits);
-#endif
+#endif /*BIG_ENDIAN*/
 }
 
-#endif /*HAVE_ENDIAN_H || HAVE_SYS_ENDIAN_H*/
+#endif /*defined(HAVE_ENDIAN_H) || defined(HAVE_SYS_ENDIAN_H) || defined(__APPLE__)*/
 
 #endif /*__SWAP_H__*/
