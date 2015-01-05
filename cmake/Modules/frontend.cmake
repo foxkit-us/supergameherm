6fb2eb4e0e4999007b1bbaea4e00b609c28c8ed6
Elizabeth Myers <elizabeth@interlinked.me>
2015-01-05 05:55:30 -0600

    Revert "Clean up cmake stuff"
    This is not working at all...
    
    This reverts commit a8469b3c84d2d7c6950df6e45857ff81e7e2fdfe.

--- cmake/Modules/frontend.cmake
+++ cmake/Modules/frontend.cmake
@@ -2,7 +2,7 @@ if(POLICY CMP0054)
 	cmake_policy(SET CMP0054 NEW)
 endif()
 
-function(libcaca_check)
+macro(libcaca_check)
 	find_package(libcaca)
 	if(libcaca_FOUND)
 		option(LIBCACA_ENABLE "Enable libcaca frontend" on)
@@ -17,16 +17,9 @@ function(libcaca_check)
 
 		set(HAVE_FRONTEND on)
 	endif()
+endmacro()
 
-	set(HAVE_LIBCACA ${HAVE_LIBCACA}
-		CACHE INTERNAL "Have libcaca frontend")
-	set(LIBCACA_ENABLE ${LIBCACA_ENABLE}
-		CACHE INTERNAL "Enable libcaca frontend")
-	set(HAVE_FRONTEND ${HAVE_FRONTEND}
-		CACHE INTERNAL "Have a frontend")
-endfunction(libcaca_check)
-
-function(sdl2_check)
+macro(sdl2_check)
 	find_package(SDL2)
 	if(SDL2_FOUND)
 		option(SDL2_ENABLE "Enable SDL2 frontend" on)
@@ -44,16 +37,9 @@ function(sdl2_check)
 
 		set(HAVE_FRONTEND on)
 	endif()
+endmacro()
 
-	set(HAVE_SDL2 ${HAVE_SDL2}
-		CACHE INTERNAL "Have SDL2 frontend")
-	set(SDL2_ENABLE ${SDL2_ENABLE}
-		CACHE INTERNAL "Enable SDL2 frontend")
-	set(HAVE_FRONTEND ${HAVE_FRONTEND}
-		CACHE INTERNAL "Have a frontend")
-endfunction(sdl2_check)
-
-function(win32_check)
+macro(win32_check)
 	if(HAVE_WINDOWS)
 		option(WIN32_ENABLE "Enable the Win32 GDI frontend" on)
 	endif()
@@ -75,16 +61,9 @@ function(win32_check)
 
 		set(HAVE_FRONTEND on)
 	endif()
+endmacro()
 
-	set(HAVE_WIN32 ${HAVE_WIN32}
-		CACHE INTERNAL "Have win32 frontend")
-	set(WIN32_ENABLE ${WIN32_ENABLE}
-		CACHE INTERNAL "Enable win32 frontend")
-	set(HAVE_FRONTEND ${HAVE_FRONTEND}
-		CACHE INTERNAL "Have a frontend")
-endfunction(win32_check)
-
-function(null_check)
+macro(null_check)
 	# Set the default value depending on if we have a frontend
 	if(HAVE_FRONTEND)
 		set(NULL_DEFAULT off)
@@ -98,11 +77,11 @@ function(null_check)
 		file(GLOB NULL_FRONTEND_SOURCES src/frontends/null/*.c)
 		add_executable("sgherm-null" ${NULL_FRONTEND_SOURCES} $<TARGET_OBJECTS:sgherm-core>)
 	endif()
-endfunction(null_check)
+endmacro()
 
 macro(frontend_checks)
 	sdl2_check()
 	libcaca_check()
 	win32_check()
 	null_check()
-endmacro(frontend_checks)
+endmacro()
--- cmake/Modules/platform.cmake
+++ cmake/Modules/platform.cmake
@@ -3,53 +3,27 @@ include(CheckFunctionExists)
 include(CheckSymbolExists)
 include(TestBigEndian)
 
-function(posix_check)
+macro(posix_check)
 	check_include_files(unistd.h HAVE_UNISTD_H)
 	if(HAVE_UNISTD_H)
 		check_symbol_exists(_POSIX_VERSION unistd.h HAVE_POSIX)
 	endif()
+endmacro()
 
-	set(HAVE_POSIX ${HAVE_POSIX}
-		CACHE INTERNAL "Operating system is POSIX-compatible")
-	set(HAVE_UNISTD_H ${HAVE_UNISTD_H}
-		CACHE INTERNAL "Have unistd.h header")
-endfunction(posix_check)
-
-function(windows_check)
+macro(windows_check)
 	check_include_files(windows.h HAVE_WINDOWS)
+endmacro()
 
-	set(HAVE_WINDOWS ${HAVE_WINDOWS}
-		CACHE INTERNAL "Operating system is Windows")
-endfunction(windows_check)
-
-function(stdc_check)
+macro(stdc_check)
 	check_include_files(stdbool.h HAVE_STDBOOL_H)
 	check_include_files(stdint.h HAVE_STDINT_H)
+endmacro()
 
-	set(HAVE_STDBOOL_H ${HAVE_STDBOOL_H}
-		CACHE INTERNAL "Have stdbool.h (C99 and later)")
-	set(HAVE_STDINT_H ${HAVE_STDINT_H}
-		CACHE INTERNAL "Have stdint.h (C99 and later)")
-endfunction(stdc_check)
-
-function(swap_check)
+macro(swap_check)
 	check_include_files(endian.h HAVE_ENDIAN_H)
 	check_include_files(sys/endian.h HAVE_SYS_ENDIAN_H)
 
-	if(HAVE_ENDIAN_H)
-		# This is sufficient
-		LIST(APPEND CMAKE_REQUIRED_DEFINITIONS "-D_BSD_SOURCE")
-		check_symbol_exists(be16toh endian.h HAVE_BSD_ENDIAN)
-	elseif(HAVE_SYS_ENDIAN_H)
-		LIST(APPEND CMAKE_REQUIRED_DEFINITIONS "-D_BSD_SOURCE")
-		check_symbol_exists(be16toh sys/endian.h HAVE_BSD_ENDIAN)
-	endif()
-
-	if(NOT HAVE_BSD_ENDIAN)
-		unset(HAVE_BSD_ENDIAN)
-		unset(HAVE_ENDIAN_H)
-		unset(HAVE_SYS_ENDIAN_H)
-
+	if(NOT(HAVE_ENDIAN_H OR HAVE_SYS_ENDIAN_H))
 		# Check for __bswap_XX
 		check_function_exists(__bswap_16 HAVE_BSWAP_16)
 		check_function_exists(__bswap_32 HAVE_BSWAP_32)
@@ -62,22 +36,9 @@ function(swap_check)
 	else()
 		add_definitions(-D_BSD_SOURCE)
 	endif()
+endmacro()
 
-	set(HAVE_ENDIAN_H ${HAVE_ENDIAN_H}
-		CACHE INTERNAL "Platform has endian.h swap builtins")
-	set(HAVE_SYS_ENDIAN_H ${HAVE_SYS_ENDIAN_H}
-		CACHE INTERNAL "Platform has sys/endian.h swap builtins")
-	set(HAVE_BSWAP_16 ${HAVE_BSWAP_16}
-		CACHE INTERNAL "Platform has __bswap_16 swap builtin")
-	set(HAVE_BSWAP_32  ${HAVE_BSWAP_32}
-		CACHE INTERNAL "Platform has __bswap_32 swap builtin")
-	set(HAVE_BYTESWAP_USHORT ${HAVE_BYTESWAP_USHORT}
-		CACHE INTERNAL "Platform has _byteswap_ushort swap builtin")
-	set(HAVE_BYTESWAP_ULONG ${HAVE_BYTESWAP_ULONG}
-		CACHE INTERNAL "Platform has _byteswap_uling swap builtin")
-endfunction(swap_check)
-
-function(clock_check)
+macro(clock_check)
 	if(HAVE_POSIX)
 		check_symbol_exists(clock_gettime time.h HAVE_CLOCK_GETTIME)
 
@@ -89,38 +50,16 @@ function(clock_check)
 		check_symbol_exists(nanosleep time.h HAVE_NANOSLEEP)
 		check_symbol_exists(clock_nanosleep time.h HAVE_CLOCK_NANOSLEEP)
 	endif()
+endmacro()
 
-	set(HAVE_NANOSLEEP ${HAVE_NANOSLEEP}
-		CACHE INTERNAL "Platform has nanosleep")
-	set(HAVE_CLOCK_NANOSLEEP ${HAVE_CLOCK_NANOSLEEP}
-		CACHE INTERNAL "Platform has clock_nanosleep")
-endfunction(clock_check)
-
-function(mmap_check)
+macro(mmap_check)
 	if(HAVE_POSIX)
 		check_symbol_exists(mmap sys/mman.h HAVE_MMAP)
 		if(HAVE_MMAP)
 			check_symbol_exists(mremap sys/mman.h HAVE_MREMAP)
 		endif()
 	endif()
-
-	set(HAVE_MMAP ${HAVE_MMAP}
-		CACHE INTERNAL "Platform has mmap")
-	set(HAVE_MREMAP ${HAVE_MREMAP}
-		CACHE INTERNAL "Platform has mremap")
-endfunction(mmap_check)
-
-function(endian_check)
-	test_big_endian(BIG_ENDIAN)
-	if(NOT BIG_ENDIAN)
-		set(LITTLE_ENDIAN 1)
-	endif()
-
-	set(BIG_ENDIAN ${BIG_ENDIAN}
-		CACHE INTERNAL "Platform is big-endian")
-	set(LITTLE_ENDIAN ${LITTLE_ENDIAN}
-		CACHE INTERNAL "Platform is little-endian")
-endfunction(endian_check)
+endmacro()
 
 macro(platform_checks)
 	posix_check()
@@ -132,9 +71,13 @@ macro(platform_checks)
 		add_definitions(${GLIBC_FEATURE})
 	endif()
 
-	endian_check()
+	test_big_endian(BIG_ENDIAN)
+	if(NOT BIG_ENDIAN)
+		set(LITTLE_ENDIAN 1)
+	endif()
+
 	stdc_check()
 	swap_check()
 	clock_check()
 	mmap_check()
-endmacro(platform_checks)
+endmacro()
