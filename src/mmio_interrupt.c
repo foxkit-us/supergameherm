2a8485395dc8ec58f815c0650656083c0b8417b1
Elizabeth Myers <elizabeth@interlinked.me>
2015-01-17 23:10:03 -0600

    Don't export these functions as static

--- src/mmio_interrupt.c
+++ src/mmio_interrupt.c
@@ -1,9 +1,9 @@
-uint8_t int_flag_read(emu_state *restrict state, uint16_t location UNUSED)
+static inline uint8_t int_flag_read(emu_state *restrict state, uint16_t location UNUSED)
 {
 	return state->interrupts.pending;
 }
 
-void int_flag_write(emu_state *restrict state, uint16_t location UNUSED, uint8_t data)
+static inline void int_flag_write(emu_state *restrict state, uint16_t location UNUSED, uint8_t data)
 {
 	state->interrupts.pending = data;
 	compute_irq(state);
