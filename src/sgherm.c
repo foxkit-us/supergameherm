a2d90a7ae3902e4a18bd500554cca9eb7ecb72d1
Elizabeth Myers <elizabeth@interlinked.me>
2015-01-18 16:52:55 -0600

    Fix a warning

--- src/sgherm.c
+++ src/sgherm.c
@@ -105,7 +105,7 @@ bool step_emulator(emu_state *restrict state)
 	// The overhead of calling execute is enough where this is worthwhile
 	if(state->wait)
 	{
-		int wait_old = state->wait;
+		unsigned wait_old = state->wait;
 		state->wait -= count_per_step_core;
 		if(unlikely(state->wait > wait_old))
 		{
